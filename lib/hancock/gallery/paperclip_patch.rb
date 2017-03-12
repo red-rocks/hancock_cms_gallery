9084c8512ff7d68d90588e90693d1c24288e886b
Alexander Kiseliev <i43ack@gmail.com>
2017-03-12 13:54:10 +0300

    inline_data fix for unexisted file

--- lib/hancock/gallery/paperclip_patch.rb
+++ lib/hancock/gallery/paperclip_patch.rb
@@ -3,8 +3,14 @@ if defined?(Paperclip)
   module Paperclip
     class Attachment
 
+      def inline_data
+        if File.exists?(self.path)
+          Paperclip.io_adapters.for(self).read.force_encoding("UTF-8").html_safe rescue ""
+        end
+      end
+
       def inline_svg
-        Paperclip.io_adapters.for(self).read.force_encoding("UTF-8").html_safe
+        inline_data if svg?
       end
 
       def svg?
