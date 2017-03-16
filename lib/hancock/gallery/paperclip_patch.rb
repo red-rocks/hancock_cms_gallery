7982c6d0666dd8a6d1683f37b75737c9155dd8ca
Alexander Kiseliev <i43ack@gmail.com>
2017-03-16 11:00:39 +0300

    Base64 for base64 and

--- lib/hancock/gallery/paperclip_patch.rb
+++ lib/hancock/gallery/paperclip_patch.rb
@@ -3,10 +3,21 @@ if defined?(Paperclip)
   module Paperclip
     class Attachment
 
-      def inline_data
-        if File.exists?(self.path)
-          Paperclip.io_adapters.for(self).read.force_encoding("UTF-8").html_safe rescue ""
+      def inline_data(style=:original)
+        if queued_for_write[style]
+          queued_for_write[style].read.force_encoding("UTF-8").html_safe rescue ""
+        elsif !path(style).blank? and File.exists?(path(style))
+          File.read(path(style)).force_encoding("UTF-8").html_safe rescue ""
         end
+      end
+
+      def base64(style=:original)
+        _data = inline_data(style)
+        Base64.encode64(_data) if _data
+      end
+      def base64_as_src(style=:original)
+        _base64 = base64(style)
+        "data:#{content_type};base64,#{_base64}" unless _base64.blank?
       end
 
       def inline_svg
