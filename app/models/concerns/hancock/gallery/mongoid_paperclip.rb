bae01c9defe50e7b767077ccab11e3f2c2e10981
Alexander Kiseliev <i43ack@gmail.com>
2018-01-31 09:22:24 +0300

    delete_ fix

--- app/models/concerns/hancock/gallery/mongoid_paperclip.rb
+++ app/models/concerns/hancock/gallery/mongoid_paperclip.rb
@@ -73,7 +73,7 @@ if Hancock.mongoid?
           end
 
           def delete_#{name}=(val)
-            if [true, 1, 't', 'true'].include?(val) # val == true or val == "1" or val == "true" or val == "t"
+            if [true, 1, 't', 'true', '1'].include?(val) # val == true or val == "1" or val == "true" or val == "t"
               #{name}.clear
             end
           end
